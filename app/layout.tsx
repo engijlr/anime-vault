029869146d16b5b6c9297074ed8523a07efbd20c
Engi Lozada <engilozada@gmail.com>
2024-04-12 21:50:04 +0200

    Adding some styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={dmSans.className}>
-        <Hero />
-        <main className="max-w-7xl mx-auto bg-[#0F1117]">{children}</main>
-        <Footer />
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children} <Footer />
+        </main>
       </body>
     </html>
   );
